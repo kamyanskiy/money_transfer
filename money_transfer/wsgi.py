c32e7141fec171ebd4ea5c928373a145dc1db1b4
Alexander Kamyanskiy <kamyanskiy@gmail.com>
2017-05-22 02:43:36 +0300

    use whitenoise in wsgi.py

--- money_transfer/wsgi.py
+++ money_transfer/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "money_transfer.settings")
+from whitenoise.django import DjangoWhiteNoise
 
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "money_transfer.settings")
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
